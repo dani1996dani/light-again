c9ebbd00a94834002650458ed11b10144101ca80
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-07 21:29:06 +0200

    arrows are moving :D

---
+++ Assets/Scripts/Movement/ArrowMovement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Movement
+{
+    public class ArrowMovement : MonoBehaviour
+    {
+        public int speed = 20;
+
+        private void Update()
+        {
+            gameObject.transform.position += Vector3.right * transform.localScale.x * speed * Time.deltaTime;
+        }
+    }
+}
