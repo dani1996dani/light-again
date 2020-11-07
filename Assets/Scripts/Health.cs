c782b2f83f7d95cfb5167a86f5e4cdf06beaf4a0
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-07 15:43:48 +0200

    added a health class with method to damage player

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Health : MonoBehaviour
+    {
+        public int curHealth = 0;
+        public int maxHealth = 100;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            curHealth = maxHealth;
+        }
+
+        public void DamagePlayer(int damage)
+        {
+            curHealth -= damage;
+
+            if(curHealth < 0)
+            {
+                Die();
+            }
+
+        }
+
+        private void Die()
+        {
+            curHealth = 0;
+            //TODO: publish event
+        }
+    }
+}
