5d697076e4f6c8e2fe278790d3242fe8d822064a
Daniel Baron <dani1996dani@gmail.com>
2020-11-07 17:05:22 +0200

    🚧	Player jumps are constricted to 1 jump at a time

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
     private float playerMovementSpeed = 15.0f;
     private Rigidbody2D playerRigidBody;
     private float jumpForce = 40.0f;
+    float distToGround;
 
     private void Awake()
     {
         playerRigidBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        distToGround = 2.1f;
     }
 
     private void Update()
@@ -27,9 +29,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        bool isPlayerGrounded = isGrounded();
+        
+        if (isPlayerGrounded)
+        {
+            Debug.Log("isPlayerGrounded" + isPlayerGrounded);
+        }
+        if (isPlayerGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
+
+    private bool isGrounded()
+    {
+        //Debug.Log("isGrounded distToGround" + distToGround);
+        //Gizmos.color = Color.red;
+        //Vector3 direction = -Vector3.up;
+        //Gizmos.DrawRay(transform.position, direction);
+
+        //Debug.Log("playersFeetCollider.transform.position" + playersFeetCollider.transform.position);
+        Debug.DrawRay(gameObject.transform.position, Vector3.down * distToGround, Color.red);
+        return Physics2D.Raycast(gameObject.transform.position, Vector3.down, distToGround, LayerMask.GetMask("Ground"));
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Vector3 direction = -Vector3.up * 5;
+    //    Gizmos.DrawRay(transform.position, direction);
+    //}
 }
