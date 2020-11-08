249cb02b5e36143f10e16bfdfdf4d14116f08794
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-08 18:05:56 +0200

    arrow is destroyed after 1 second, and on collision. also gives 10 damage.

---
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class Arrow : MonoBehaviour
+    {
+        private float timeToLive = 1.0f;
+        private int damage = 10;
+
+        private void Update()
+        {
+            timeToLive -= Time.deltaTime;
+            if(timeToLive <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter(Collider col)
+        {
+            Destroy(gameObject);
+            col.gameObject.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
