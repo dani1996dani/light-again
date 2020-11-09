def3b9bb491b1b00f6656d2858284b70186d3bc2
Daniel Baron <dani1996dani@gmail.com>
2020-11-09 22:47:01 +0200

    LA-43 🔩	added layer ignore collision. Chasing works.

--- Assets/Scripts/Movement/OwlmanChaseMovement.cs
+++ Assets/Scripts/Movement/OwlmanChaseMovement.cs
@@ -19,24 +19,31 @@ public class OwlmanChaseMovement : MonoBehaviour {
 
     private void Start() {
         // OwlmanStartChasingVisionRange
+        
     }
 
     private void Update() {
         if(isPlayerVisibleOnLeftSide()){
             Debug.Log("Player on the Left!1");
+            Chase(Vector3.left);
         }
         if(isPlayerVisibleOnRightSide()){
             Debug.Log("Player on the right!1");
+            Chase(Vector3.right);
         }
-        
+    }
+
+    public void Chase(Vector3 direction)
+    {
+        gameObject.transform.position += direction * Time.deltaTime * Settings.OwlmanChaseSpeed;
     }
     
-    private bool isPlayerVisibleOnLeftSide(){
+    public bool isPlayerVisibleOnLeftSide(){
         Debug.DrawRay(leftHandGameObject.transform.position, Vector3.left * 10, Color.green);
         return Physics2D.Raycast(leftHandGameObject.transform.position, Vector2.left, Settings.OwlmanStartChasingVisionRange, LayerMask.GetMask("Player"));
     }
 
-    private bool isPlayerVisibleOnRightSide(){
+    public bool isPlayerVisibleOnRightSide(){
         Debug.DrawRay(leftHandGameObject.transform.position, Vector3.right * 10, Color.blue);
         return Physics2D.Raycast(rightHandGameObject.transform.position, Vector2.right, Settings.OwlmanStartChasingVisionRange, LayerMask.GetMask("Player"));
     }
