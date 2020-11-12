3b728064bc3d8d67fd4656648a01983931e2cfb6
Yolanda <yogo456@gmail.com>
2020-11-12 22:20:24 +0200

    🦉 owlman attack publishes player hit event, player damage subsriber gets the event and calls player health to take damage

---
+++ Assets/Scripts/PlayerDamageSubsriber.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class PlayerDamageSubsriber : MonoBehaviour
+{
+    private Health playerHealth;
+
+    void Awake()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag(Settings.TagPlayer).GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        Publisher.publish.PlayerHit += OnPlayerHit;
+    }
+
+    private void OnPlayerHit(int damage)
+    {
+        playerHealth.TakeDamage(damage);
+    }
+
+    private void OnDisable()
+    {
+        Publisher.publish.PlayerHit -= OnPlayerHit;
+    }
+}
