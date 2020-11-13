a3c4f83adc985b4518b6381d63ebb851aee41732
Daniel Baron <dani1996dani@gmail.com>
2020-11-13 18:23:05 +0200

    LA-59 🐕	fade in works, and it looks amazing :)

---
+++ Assets/Scripts/Fade/FadeInOnStart.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Fade
+{
+    class FadeInOnStart : MonoBehaviour
+    {
+        SpriteRenderer spriteRenderer;
+        float smoothTime = Settings.TimeTillMoonDustCollectionTriggerIsActive;
+        float velocity = 0.0f;
+        float alpha;
+        float alphaTarget = 1;
+        float acceptableOffest = 0.1f;
+
+        private void Start()
+        {
+            spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+            SetAlpha(0);
+        }
+
+        private void Update()
+        {
+            alpha = Mathf.SmoothDamp(alpha, alphaTarget, ref velocity, smoothTime);
+            Debug.Log("alpha " + alpha);
+            SetAlpha(alpha);
+            if(alphaTarget - acceptableOffest <= alpha)
+            {
+                SetAlpha(alphaTarget);
+                this.enabled = false;
+            }
+        }
+
+        private void SetAlpha(float newAlpha)
+        {
+            Color color = spriteRenderer.color;
+            color.a = newAlpha;
+            spriteRenderer.color = color;
+        }
+    }
+}
