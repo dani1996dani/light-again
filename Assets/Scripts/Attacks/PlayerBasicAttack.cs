88b78b1ed4629ee53b7783417a0376d3615a9712
Daniel Baron <dani1996dani@gmail.com>
2020-11-15 21:51:32 +0200

    LA-61 🐙	wip

--- Assets/Scripts/Attacks/PlayerBasicAttack.cs
+++ Assets/Scripts/Attacks/PlayerBasicAttack.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
 
 namespace Assets.Scripts.Attacks
 {
@@ -9,12 +11,16 @@ namespace Assets.Scripts.Attacks
         private PlayerMovement playerMovement;
         private bool isShootingRight = true;
         private float cooldown = 0.0f;
+        private Animator playerAnimator;
+        AnimationClip attackAnimationClip;
 
         private void Awake()
         {
             arrowPrefab = (GameObject)Resources.Load("Prefabs/Arrow", typeof(GameObject));
             arrowSpawningPosition = GameObject.FindGameObjectWithTag("ArrowSpawningPosition");
             playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+            playerAnimator = GameObject.FindGameObjectWithTag(Settings.TagPlayer).GetComponentInChildren<Animator>();
+            attackAnimationClip = playerAnimator.runtimeAnimatorController.animationClips.FirstOrDefault((x) => x.name == "Attack");
         }
 
         private void Update()
@@ -22,17 +28,42 @@ namespace Assets.Scripts.Attacks
             cooldown -= Time.deltaTime;
             if (!Settings.isGamePaused && Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
             {
-                isShootingRight = playerMovement.isFacingRight;
-                GameObject arrow = Instantiate(arrowPrefab, arrowSpawningPosition.transform.position, Quaternion.identity);
+                StartCoroutine("ToggleAttackAnimation");
+                StartCoroutine("SpawnArrow");
+                //isShootingRight = playerMovement.isFacingRight;
+                //GameObject arrow = Instantiate(arrowPrefab, arrowSpawningPosition.transform.position, Quaternion.identity);
 
-                if (!isShootingRight)
-                {
-                    Vector3 vector = arrow.transform.localScale;
-                    vector.x = -1;
-                    arrow.transform.localScale = vector;
-                }
+                //StartCoroutine("ToggleAttackAnimation");
+                //if (!isShootingRight)
+                //{
+                //    Vector3 vector = arrow.transform.localScale;
+                //    vector.x = -1;
+                //    arrow.transform.localScale = vector;
+                //}
                 cooldown = 0.5f;
             }
+        }
+
+        IEnumerator SpawnArrow()
+        {
+            yield return new WaitForSeconds(attackAnimationClip.length / 2);
+            isShootingRight = playerMovement.isFacingRight;
+            GameObject arrow = Instantiate(arrowPrefab, arrowSpawningPosition.transform.position, Quaternion.identity);
+
+            
+            if (!isShootingRight)
+            {
+                Vector3 vector = arrow.transform.localScale;
+                vector.x = -1;
+                arrow.transform.localScale = vector;
+            }
+        }
+
+        IEnumerator ToggleAttackAnimation()
+        {
+            playerAnimator.SetBool("isAttacking", true);
+            yield return new WaitForSeconds(attackAnimationClip.length);
+            playerAnimator.SetBool("isAttacking", false);
         }
     }
 }
