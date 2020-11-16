0d95d637c735dd46219d2db9c76314328a3663bd
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-16 18:11:09 +0200

    LA-46 🐈	pr (#30)

---
+++ Assets/MoonPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonPosition : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 moonWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.085f, Screen.height * 0.85f, 5));
+        transform.position = moonWorldPoint;
+    }
+}
