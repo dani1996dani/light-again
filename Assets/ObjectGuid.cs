475ad1706b898297c89204fa4467b603f07a1eb7
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-19 22:43:14 +0200

    LA-49 🐇	prc (#35)
    * LA-49 🐇	prc
    
    * LA-49 🐥	pr fixes

---
+++ Assets/ObjectGuid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGuid : MonoBehaviour
+{
+    int guid;
+    
+    void Start()
+    {
+        guid = Guid.NewGuid().GetHashCode();
+    }
+
+    public int GetGuid()
+    {
+        return this.guid;
+    }
+}
--- Assets/Scripts/Movement/PlayerMoonDustPickup.cs
+++ Assets/Scripts/Movement/PlayerMoonDustPickup.cs
@@ -1,19 +1,35 @@
 using Assets.Scripts;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerMoonDustPickup : MonoBehaviour
 {
+    MoonDustProgress moonDustProgressController;
+    HashSet<int> collectedMoonDustGuids = new HashSet<int>();
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject colGameObject = collision.gameObject;
         if (colGameObject.tag == Settings.TagMoonDust)
-        {
-            CollectMoonDust(colGameObject);
+        {    
+            int moonDustGuid = colGameObject.GetComponent<ObjectGuid>().GetGuid();
+            if (!collectedMoonDustGuids.Contains(moonDustGuid))
+            {
+                CollectMoonDust(colGameObject, moonDustGuid);
+            }
         }
     }
 
-    private void CollectMoonDust(GameObject moonDust)
+    private void CollectMoonDust(GameObject moonDust, int moonDustGuid)
     {
+        collectedMoonDustGuids.Add(moonDustGuid);
+
+        if(moonDustProgressController == null)
+        {
+            moonDustProgressController = GameObject.FindGameObjectWithTag(Settings.TagGameSettings).GetComponent<MoonDustProgress>();
+        }
+        moonDustProgressController.IncrementMoonDustAmount();
+
         Destroy(moonDust);
     }
 }
