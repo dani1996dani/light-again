e20ea07f83be9d76624e58c3922499d673e269da
Daniel Baron <dani1996dani@gmail.com>
2020-11-22 21:39:46 +0200

    LA-19 🐓	prc

--- Assets/Scripts/Attacks/PlayerStarStrike.cs
+++ Assets/Scripts/Attacks/PlayerStarStrike.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts;
 using Assets.Scripts.Attacks;
+using Assets.Scripts.Movement;
 using System.Linq;
 
 public class PlayerStarStrike : MonoBehaviour
@@ -12,6 +13,8 @@ public class PlayerStarStrike : MonoBehaviour
     private PlayerBasicAttack playerBasicAttackController;
     PlayerMovement playerMovementController;
 
+    private GameObject starStrikeArrowPrefab;
+
     void Start()
     {
         GameObject playerGameObject = GameObject.FindGameObjectWithTag(Settings.TagPlayer);
@@ -19,6 +22,8 @@ public class PlayerStarStrike : MonoBehaviour
         playerBasicAttackController = playerGameObject.GetComponentInChildren<PlayerBasicAttack>();
         starStrikeAnimationClip = playerAnimator.runtimeAnimatorController.animationClips.FirstOrDefault((x) => x.name == "StarStrike");
         playerMovementController = playerGameObject.GetComponent<PlayerMovement>();
+
+        starStrikeArrowPrefab = (GameObject)Resources.Load("Prefabs/StarStrikeArrow", typeof(GameObject));
     }
 
     void Update()
@@ -33,10 +38,20 @@ public class PlayerStarStrike : MonoBehaviour
         }
     }
 
+    public void SpawnVerticalArrow()
+    {
+        GameObject arrow = Instantiate(starStrikeArrowPrefab, transform.position, Quaternion.identity);
+        ArrowMovement arrowMovement = arrow.GetComponent<ArrowMovement>();
+        arrowMovement.SetDirection(Vector3.up);
+    }
+
     IEnumerator CastStarStrike()
     {
         playerBasicAttackController.SetIsAttacking(true);
         playerAnimator.SetBool("isCastingStarStrike", true);
+
+        SpawnVerticalArrow();
+
         yield return new WaitForSeconds(starStrikeAnimationClip.length);
         playerBasicAttackController.SetIsAttacking(false);
         playerAnimator.SetBool("isCastingStarStrike", false);
