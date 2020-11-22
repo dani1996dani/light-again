6758ac1a835890b33aed0d67afdfbe2506bfda4c
Daniel Baron <dani1996dani@gmail.com>
2020-11-22 21:57:20 +0200

    LA-28 🐈	working event when star strike should happen

---
+++ Assets/StarStrikeListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class StarStrikeListener : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        Publisher.publish.StarStrike += OnStarStrike;
+    }
+
+    void OnDisable()
+    {
+        Publisher.publish.StarStrike -= OnStarStrike;
+    }
+
+    private void OnDestroy()
+    {
+        Publisher.publish.StarStrike -= OnStarStrike;
+    }
+
+    void OnStarStrike()
+    {
+        Debug.Log("STAR STRIKE!!");
+    }
+}
