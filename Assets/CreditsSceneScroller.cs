e2b2b31196d0d5d6ea157d7501aadbdf30bcc477
Daniel Baron <dani1996dani@gmail.com>
2020-11-28 00:05:14 +0200

    LA-54 🐅	added text for credits

---
+++ Assets/CreditsSceneScroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsSceneScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currPosition = transform.position;
+        transform.position = new Vector3(currPosition.x, currPosition.y + speed * Time.deltaTime, currPosition.z);
+    }
+}
