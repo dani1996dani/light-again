6cf090a07f8796c8da5faf98b0d49fc86bcf8324
Daniel Baron <dani1996dani@gmail.com>
2020-11-28 00:22:25 +0200

    LA-54 🐔	prc

--- Assets/CreditsSceneScroller.cs
+++ Assets/CreditsSceneScroller.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.SceneManagement;
 
 public class CreditsSceneScroller : MonoBehaviour
 {
     [SerializeField]
     private float speed = 50f;
+
+    private void Start()
+    {
+        //StartCoroutine("GoToMainMenu");
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,4 +20,11 @@ public class CreditsSceneScroller : MonoBehaviour
         Vector3 currPosition = transform.position;
         transform.position = new Vector3(currPosition.x, currPosition.y + speed * Time.deltaTime, currPosition.z);
     }
+
+    //IEnumerator GoToMainMenu()
+    //{
+    //    yield return new WaitForSeconds(40.0f);
+    //    SceneChanger sceneChanger = GameObject.FindGameObjectWithTag(Settings.TagSceneChanger).GetComponent<SceneChanger>();
+    //    sceneChanger.GoToLevel(0);
+    //}
 }
