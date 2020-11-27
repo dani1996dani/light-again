f1ff20f4d1c5d74229882fdfc3a33f5fb9f92494
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-28 00:24:06 +0200

    Feat/la 54/credits scene (#67)
    * LA-54 🐅	added text for credits
    
    * LA-54 🐔	prc
    
    * Update CreditsSceneScroller.cs

---
+++ Assets/CreditsSceneScrollEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneScrollEnd : MonoBehaviour
+{
+    bool didTransitionToMainMenu = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!didTransitionToMainMenu)
+        {
+            SceneChanger sceneChanger = GameObject.FindGameObjectWithTag(Settings.TagSceneChanger).GetComponent<SceneChanger>();
+            sceneChanger.GoToLevel(0);
+            didTransitionToMainMenu = true;
+        }
+    }
+}
---
+++ Assets/CreditsSceneScroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 50f;
+
+    void Update()
+    {
+        Vector3 currPosition = transform.position;
+        transform.position = new Vector3(currPosition.x, currPosition.y + speed * Time.deltaTime, currPosition.z);
+    }
+}
