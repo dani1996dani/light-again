dd8cfff9f8cc95346623b786fcb51bb9ece2ef22
Daniel <30955288+dani1996dani@users.noreply.github.com>
2020-11-28 00:23:39 +0200

    Update CreditsSceneScroller.cs

--- Assets/CreditsSceneScroller.cs
+++ Assets/CreditsSceneScroller.cs
@@ -9,22 +9,9 @@ public class CreditsSceneScroller : MonoBehaviour
     [SerializeField]
     private float speed = 50f;
 
-    private void Start()
-    {
-        //StartCoroutine("GoToMainMenu");
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 currPosition = transform.position;
         transform.position = new Vector3(currPosition.x, currPosition.y + speed * Time.deltaTime, currPosition.z);
     }
-
-    //IEnumerator GoToMainMenu()
-    //{
-    //    yield return new WaitForSeconds(40.0f);
-    //    SceneChanger sceneChanger = GameObject.FindGameObjectWithTag(Settings.TagSceneChanger).GetComponent<SceneChanger>();
-    //    sceneChanger.GoToLevel(0);
-    //}
 }
